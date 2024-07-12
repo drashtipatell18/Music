9f32e904b5361069d52063ec9c2b73bd528606e1
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-07-12 17:54:12 +0530

    auth code setup

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ForgotPasswordMail;
+use Illuminate\Support\Facades\Session;
+
+class DashboardController extends Controller
+{
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+    public function showForgetPasswordForm()
+    {
+        return view('auth.forgetpass');
+    }
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $email = $request->email;
+
+        $user = User::where('email', $email)->first();
+
+        if ($user) {
+            // OTP generate
+            $otp = mt_rand(100000, 999999);
+            $user->otp = $otp;
+
+            session(['otp' => $otp]);
+
+            $user->remember_token = Str::random(40);
+            $user->save();
+
+            session(['reset_email' => $user->email]);
+
+            Mail::to($user->email)->send(new ForgotPasswordMail($user));
+            return redirect()->route('verify.otp')->with('success', 'OTP sent successfully.');
+        } else {
+            return back()->with('danger', 'User not found.');
+        }
+    }
+    public function verifyOTPForm()
+    {
+        $email = session('reset_email');
+        if (!$email) {
+            return redirect()->route('show.forgetpassword')->with('danger', 'Session expired. Please try again.');
+        }
+        return view('auth.otpPage', ['email' => $email]);
+    }
+
+    public function verifyOTP(Request $request)
+    {
+        $email = session('reset_email');
+        $user = User::where('email', $email)->first();
+        $otp = $user->otp;
+
+        if ($request->otp == $otp) {
+            // OTP is valid
+           dd('success');
+        }
+
+
+
+
+        return back()->withErrors(['otp' => 'Invalid OTP.']);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    if(Auth::check()) {
+        return redirect()->route('login');
+    }
+    return redirect()->route('dashboard');
+});
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/create/register', [RegisterController::class, 'register'])->name('create.register');
+
+Route::get('/login', [HomeController::class, 'Login'])->name('login');
+Route::post('/loginstore', [HomeController::class, 'loginStore'])->name('loginstore');
+Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
+
+Route::get('/forget-password', [DashboardController::class, 'showForgetPasswordForm'])->name('forget.password');
+Route::post('/forget-password', [DashboardController::class, 'sendResetLinkEmail'])->name('forget.password.email');
+Route::get('/verify-otp', [DashboardController::class, 'verifyOTPForm'])->name('verify.otp');
+Route::post('verify-otp', [DashboardController::class, 'verifyOTP']);
+
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+Auth::routes([
+    'register' => true,
+    'reset' => false,
+    'verify' => false
+]);
