088653b8a5b80e6464b60e5265ec3d6b0f739660
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-07-15 13:23:48 +0530

    Update HomeController.php
    new changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -17,55 +13,6 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-    }
-
-    public function Register()
-    {
-        return view('auth.register');
-    }
-
-    public function RegisterStore(Request $request)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-            'confirm_password' => 'required|same:password'
-        ]);
-
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-        ]);
-    }
-
-    // public function Login(){
-    //     return view('auth.login');
-    // }
-
-    // public function loginStore(Request $request)
-    // {
-    //     $credentials = $request->validate([
-    //         'email' => 'required|email',
-    //         'password' => 'required',
-    //     ]);
-
-    //     // Attempt to authenticate using the user's email
-    //     if (Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
-    //         return redirect()->route('dashboard');
-    //     }
-
-    //     // If none of the attempts succeed, redirect back with an error message
-    //     return back()->withErrors(['email' => 'Invalid credentials'])->withInput($request->only('email'));
-    // }
-
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect('login');
     }
 
 }
